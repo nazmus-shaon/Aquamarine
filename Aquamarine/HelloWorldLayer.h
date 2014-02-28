1f2a7345a7484904b5f7a7e6c267625262011ffe
shaon <nazmus.shaon@tum.de>
2014-02-28 14:58:56 +0100

    Initializing Aquamarine

---
+++ Aquamarine/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Aquamarine
+//
+//  Created by Nazmus Shaon on 28/02/14.
+//  Copyright Grasshopper 2014. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
